10ab575fda68ac882151d537642dbd1bb9b543ad
Diskouna GNANGUESSIM <gnanguessim@insa-toulouse.fr>
2024-10-26 12:25:49 +0200

    Clean the repository

--- NAND.v
+++
@@ -1,8 +0,0 @@
-module top_module (input a, input b, input c, output out);//
-	
-    wire o;
-    assign out = ~o;
-    
-    andgate inst1 ( .a(a), .b(b), .c(c), .out(o), .d(1'b1), .e(1'b1));
-
-endmodule
