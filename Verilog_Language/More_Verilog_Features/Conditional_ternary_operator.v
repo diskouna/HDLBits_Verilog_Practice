05d22f3a4cba35c772dbe4411dec7eee2f414f8e
Diskouna GNANGUESSIM <gnanguessim@insa-toulouse.fr>
2024-10-26 12:21:33 +0200

    Clean the repository

--- Verilog_Language/More_Verilog_Features/Conditional_ternary_operator.v
+++
@@ -1,8 +0,0 @@
-module top_module (
-    input [7:0] a, b, c, d,
-    output [7:0] min);//
-
-    wire [7:0] tmp1 = (a < b) ? a : b;
-    wire [7:0] tmp2 = (c < d) ? c : d;
-    assign min = (tmp1 < tmp2) ? tmp1 : tmp2;
-endmodule
